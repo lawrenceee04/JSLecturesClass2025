652c241e1d09bbe53d09005f62120e48006c9aef
lawrenceee04 <lawrencelg04@gmail.com>
2025-02-05 13:19:55 +0800

    Challenge 6 Done

---
+++ 04-logic-control-flow/04-challenge/script.js
@@ -0,0 +1,20 @@
+const calculator = (num1, num2, op) => {
+    switch (op) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            return 'ERROR: Invalid operator';
+    }
+};
+
+console.log(calculator(5, 2, '+'));
+console.log(calculator(5, 2, '-'));
+console.log(calculator(5, 2, '*'));
+console.log(calculator(5, 2, '/'));
+console.log(calculator(5, 2, '&'));
